22cbc40e7b6bf88712e61e049555bedbb1144365
Anneshabhakta2005 <anneshabhakta2005@gmail.com>
2025-09-14 19:24:36 +0530

    Event page added

--- src/assets/fake-data/data-card.js
+++ src/assets/fake-data/data-card.js
@@ -6,6 +6,14 @@ const img4 = '/images2/new/card3.webp'
 const img5 = '/images2/new/card5.webp'
 const img6 = '/images2/new/card7.webp'
 const img7 = '/images2/new/card8.webp'
+const img8 = '/images2/new/card9.webp'
+const img9 = '/images2/new/card10.webp'
+const img10 = '/images2/new/card11.webp'
+const img11 = '/images2/new/card12.webp'
+const img12 = '/images2/new/card13.webp'
+const img13 = '/images2/new/card14.webp'
+const img14 = '/images2/new/card15.webp'
+const img15 = '/images2/new/card16.webp'
 
 
 const logo = '/images2/new/newlogo.webp'
@@ -68,17 +76,79 @@ const dataCard = [
         name: 'Team Samarth',
         tag: '@ContentTeam'
     },
+   
     {
-        id: 7,
-        img: img6,
-        title: 'Enthusia',
-        
+        id: 8,
+        img: img8,
+        title: 'Apti Acumen',
+    
+        avt: logo,
+        name: 'Team Samarth',
+        tag: '@Safalya25'
+    },  
+    {
+        id: 9,
+        img: img9,
+        title: 'Matrix Of Mock',
+
         avt: logo,
         name: 'Team Samarth',
         tag: '@Samarth'
     },
+    {
+        id: 10,
+        img: img10,
+        title: 'Re-Imagine a Book Cover',
 
+        avt: logo,
+        name: 'Team Samarth',
+        tag: '@Safalya25'
+    },
+    {
+        id: 11,
+        img: img11,
+        title: 'UI Design-Ed',
 
+        avt: logo,
+        name: 'Team Samarth',
+        tag: '@Safalya25'
+    },
+    {
+        id: 12,
+        img: img12,
+        title: 'Snap Flicks',   
+
+        avt: logo,
+        name: 'Team Samarth',
+        tag: '@Safalya25'
+    },
+    {
+        id: 13,
+        img: img13,
+        title: 'Sherlocks Escape',
+        
+        avt: logo,
+        name: 'Team Samarth',
+        tag: '@Safalya25'
+    },
+    {
+        id: 14,
+        img: img14,
+        title: 'Brain Blitz',
+
+        avt: logo,
+        name: 'Team Samarth',
+        tag: '@Safalya25'
+    },
+    {
+        id: 15,
+        img: img15,
+        title: 'Triathlon',
+
+        avt: logo,
+        name: 'Team Samarth',
+        tag: '@Safalya25'
+    },
 
 ]
 
